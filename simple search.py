bcf00684b4fbf2ff559f6d31b62dc25beda0b169
Utkarsh Srivastava <55698308+Awakening1-prog@users.noreply.github.com>
2020-12-19 17:28:38 +0530

    first commit

---
+++ simple search.py
@@ -0,0 +1,7 @@
+#https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/description/
+
+
+t=int(input())
+arr=list(int(x) for x in input().split())
+tar=int(input())
+print(arr.index(tar))
